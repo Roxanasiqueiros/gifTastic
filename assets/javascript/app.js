0ca970de14f3635c82dbe68202298b393bd5b722
Roxana Siqueiros <siqueirosroxana@yahoo.com>
2019-08-12 10:22:34 -0700

    updated background,index html, js code, assets code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,73 +1,99 @@
 let topics = ["Cherry Blossoms", "Roses", "Sunflowers","Lilies", "Lotus", "Peony", "Daisy", "Sweet pea"];
-let numberOfGIFs = 10;
-let cutOffRating = "PG";
 
-function renderButtons(){
-	for(let i = 0; i < topics.length; i++) {
-		let newButton = $("<button>");
-		newButton.addClass("btn");
-		newButton.addClass("flower-button");
-		newButton.text(topics[i]);
-		$("#button-container").append(newButton);
-	}
-	$(".flower-button").unbind("click");
+$(document).ready(function() {
+    renderButton();
+    function renderButton() {
+        $("#allbuttons").empty();
 
-	$(".flower-button").on("click", function(){
-		$(".gif-image").unbind("click");
-		$("#gif-container").empty();
-		$("#gif-container").removeClass("dotted-border");
-		populateGIFContainer($(this).text());
-	});
+        for (let i=0 ; i < topics.length; i++) {
 
-}
+            let newButton = $("<button>");
+            newButton.addClass("itembutton");
+            newButton.addClass("btn btn-success");
+            newButton.text(topics[i]);
+            newButton.attr("data-name", topics[i]);
+            $("#allbuttons").append(newButton);
 
-function addButton(show){
-	if(topics.indexOf(show) === -1) {
-		topics.push(show);
-		$("#button-container").empty();
-		renderButtons();
-	}
-}
+        }
 
-function populateGIFContainer(show){
-	$.ajax({
-		url: "https://api.giphy.com/v1/gifs/search?q=" + show + 
-		"&api_key=WW5ClgnRWArRiiIE1RB5h1iwd5Cp3tC9mt&rating" + cutOffRating + "&limit=" + numberOfGIFs,
-		method: "GET"
-	}).then(function(response){
-		response.data.forEach(function(element){
-			newDiv = $("<div>");
-			newDiv.addClass("individual-gif-container");
-			newDiv.append("<p>Rating: " + element.rating.toUpperCase() + "</p>");
-			let newImage = $("<img src = '" + element.images.fixed_height_still.url + "'>");
-			newImage.addClass("gif-image");
-			newImage.attr("state", "still");
-			newImage.attr("still-data", element.images.fixed_height_still.url);
-			newImage.attr("animated-data", element.images.fixed_height.url);
-			newDiv.append(newImage);
-			$("#gif-container").append(newDiv);
-		});
-		
-		$("#gif-container").addClass("dotted-border");
-		$(".gif-image").unbind("click");
-		$(".gif-image").on("click", function(){
-			if($(this).attr("state") === "still") {
-				$(this).attr("state", "animated");
-				$(this).attr("src", $(this).attr("animated-data"));
-			}
-			else {
-				$(this).attr("state", "still");
-				$(this).attr("src", $(this).attr("still-data"));
-			}
-		});
-	});
-}
+    }
 
-$(document).ready(function(){
-	renderButtons();
-	$("#submit").on("click", function(){
-		event.preventDefault();
-		addButton($("#different-flowers").val().trim());
-		$("#different-flowers").val("");
-	});
-});
+
+
+    $("#addbutton").on("click",  function(event) {
+        
+        event.preventDefault();
+        let addedData = $("#userinput").val().trim();
+        if (addedData != "") {
+            topics.push(addedData);
+            renderButton();
+            $("#userinput").val(" ");
+        }
+
+
+
+    });  
+
+    $(document).on("click", ".itembutton", displayInfo);
+
+
+    function displayInfo() {
+        let itemName = $(this).attr("data-name");
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=flowers+" + itemName + "&rating=g&limit=15&api_key=WW5ClgnRWArRiiIE1RB5h1iwd5Cp3tC9";
+        $("#mainimages").empty();
+
+        $.ajax ({
+            url: queryURL,
+            method: "GET"
+        }) .then(function(response) {
+            console.log(response);
+
+            let results = response.data;
+
+            for (let i=0; i<results.length; i++) {
+
+                let dataImage = $("<img>");
+                dataImage.attr("src", results[i].images.fixed_height_still.url);
+                dataImage.attr("data-still", results[i].images.fixed_height_still.url);
+                dataImage.attr("data-animate", results[i].images.fixed_height.url);
+                dataImage.addClass("gif");
+                dataImage.attr("data-state", "still");
+    
+    
+                let newItemdiv = $('<div class="newItem">');
+                let gifRating = results[i].rating;
+                let divRating = $("<p>").text("Rating: " + gifRating);
+                
+                newItemdiv.append(divRating);
+                newItemdiv.append(dataImage);
+    
+                $("#mainimages").prepend(newItemdiv);
+
+
+
+            }
+    
+ 
+        }); 
+
+
+    }
+
+
+    $("#mainimages").on("click", ".gif", function() {
+        let state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        }
+
+
+        else if (state === "animate") {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+
+    });
+
+
+})
